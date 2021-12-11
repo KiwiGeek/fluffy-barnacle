8b508aadf0bdb6be457a7199b8f493d0c0ffcde9
KiwiGeek <jpenman@hotmail.com>
2021-12-11 08:56:39 -0500

    2015: Day 3

--- TwentyFifteen/Program.cs
+++ TwentyFifteen/Program.cs
@@ -46,6 +46,53 @@
     return (totalPaper, totalRibbon);
 }
 
+(ulong year1, ulong year2) Day3()
+{
+
+    string input = File.ReadAllText("Assets\\day3.txt");
+    Dictionary<(int x, int y), bool> visited = new();
+    Dictionary<(int x, int y), bool> altVisited = new();
+    bool altTurn = false;
+
+    // take care of the implicit first house
+    (int x, int y) currentLocation = new(0, 0);
+    (int x, int y) altOneLocation = new(0, 0);
+    (int x, int y) altTwoLocation = new(0, 0);
+    visited[currentLocation] = true;
+
+    foreach (char c in input)
+    {
+        if (c == '^') { currentLocation.y--; }
+        if (c == 'v') { currentLocation.y++; }
+        if (c == '<') { currentLocation.x--; }
+        if (c == '>') { currentLocation.x++; }
+
+        if (altTurn)
+        {
+            if (c == '^') { altOneLocation.y--; }
+            if (c == 'v') { altOneLocation.y++; }
+            if (c == '<') { altOneLocation.x--; }
+            if (c == '>') { altOneLocation.x++; }
+        }
+        else 
+        {
+            if (c == '^') { altTwoLocation.y--; }
+            if (c == 'v') { altTwoLocation.y++; }
+            if (c == '<') { altTwoLocation.x--; }
+            if (c == '>') { altTwoLocation.x++; }
+        }
+        altVisited[altOneLocation] = true;
+        altVisited[altTwoLocation] = true;
+        altTurn = !altTurn;
+
+        visited[currentLocation] = true;
+    }
+
+    return ((ulong)visited.Count, (ulong)altVisited.Count);
+
+}
+
+
 (long floor, ulong firstBasement) = Day1();
 Console.WriteLine("Day 1");
 Console.WriteLine($"  Part 1 - Final Floor: {floor}");
@@ -54,3 +101,7 @@ Console.WriteLine($"  Part 2 - First Step for Basement: {firstBasement}");
 Console.WriteLine("Day 2");
 Console.WriteLine($"  Part 1 - Square Feet of Paper: {squareFeetOfPaper}");
 Console.WriteLine($"  Part 2 - Feet of Ribbon: {feetOfRibbon}");
+(ulong year1, ulong year2) = Day3();
+Console.WriteLine("Day 3");
+Console.WriteLine($"  Part 1 - Houses Delivered To Year 1: {year1}");
+Console.WriteLine($"  Part 2 - Houses Delivered To Year 2: {year2}");
