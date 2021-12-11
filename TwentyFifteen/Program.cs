dd90efff0f03f1c8fe2f1b104f5f1105c8e19e4c
KiwiGeek <jpenman@hotmail.com>
2021-12-10 22:37:53 -0500

    Day 1

---
+++ TwentyFifteen/Program.cs
@@ -0,0 +1,21 @@
+﻿(long floor, ulong firstBasement) = Day1();
+Console.WriteLine("Day 1");
+Console.WriteLine($"  Part 1 - Final Floor: {floor}");
+Console.WriteLine($"  Part 2 - First Step for Basement: {firstBasement}");
+
+(long floor, ulong firstBasementStep) Day1()
+{
+    string input = File.ReadAllText("Assets\\day1.txt");
+    long floor = 0;
+    ulong firstBasementIndex = 0;
+    ulong index = 0;
+
+    foreach (char c in input)
+    {
+        index++;
+        floor += c == '(' ? 1 : -1;
+        if (floor < 0 && firstBasementIndex == 0) { firstBasementIndex = index; }
+    }
+
+    return (floor, firstBasementIndex);
+}
